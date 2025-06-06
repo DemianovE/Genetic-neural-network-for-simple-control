276ed27da73e6566c59fd37ebefcd65bf2d45d32
DemianovE <work.demianov@gmail.com>
2025-06-06 13:03:03 +0200

    refactor pid controller testing 1) make it use unity 2) some errors correction

--- test/tests/general/test_pid_controller.c
+++ test/tests/general/test_pid_controller.c
@@ -1,53 +1,45 @@
 #include "general/test_pid_controller.h"
 
 #include "general/pid_controller.h"
-#include "general/ploting_toolbox.h"
+#include "general/plotting_toolbox.h"
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "unity/unity.h"
 
-// used to print testing outputs
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_BOLD         "\x1b[1m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+void setUp() {}
+void tearDown() {}
 
-int testPIDCreate(){
-  printf(ANSI_BOLD "=======TEST PID CREATE STARTED=======" ANSI_COLOR_RESET "\n");
-
-  struct PID *pid = (struct PID*)malloc(sizeof(struct PID));
+void testPIDCreate(){
+  PID *pid = malloc(sizeof(struct PID));
   createNewPidController(pid);
 
   FILE *csvFile = fopen("data_pid_.csv", "w");
   fprintf(csvFile, "P,I,D,CV,RV\n");
 
-  pid->Kp = 1.0;
-  pid->Ki = 1.0;
-  pid->Kd = 0.0;
+  pid->Kp = 1;
+  pid->Ki = 1;
+  pid->Kd = 0;
 
-  pid->limMax = 10.0;
-  pid->limMin = -10.0;
+  pid->limMax = 10;
+  pid->limMin = -10;
 
-  pid->limMaxInt = pid->limMax / 2.0;
-  pid->limMinInt = pid->limMin / 2.0;
+  pid->limMaxInt = pid->limMax / 2;
+  pid->limMinInt = pid->limMin / 2;
 
-  int csv = 1;
-
-  makeSimulationOfSignal(pid, csvFile, csv);
+  makeSimulationOfSignal(pid, csvFile, 1);
 
   printf("%f\n",pid->fit);
-
   plotGraph();
 
   deletePid(pid);
-  int flag = 1;
-
   fclose(csvFile);
+}
 
-  if(flag == 0){
-    printf(ANSI_BOLD ANSI_COLOR_RED "=======TEST PID CREATE FAILED=======" ANSI_COLOR_RESET "\n");
-    return 0;
-  }
-  printf(ANSI_BOLD ANSI_COLOR_GREEN "=======TEST PID CREATE SUCCESSFUL=======" ANSI_COLOR_RESET "\n");
-  return 1;
+int main(void) {
+  UNITY_BEGIN();
+
+  RUN_TEST(testPIDCreate);
+
+  return UNITY_END();
 }
