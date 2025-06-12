f8f1ff88edd7474df2af5e600dc28b7f630d40db
DemianovE <work.demianov@gmail.com>
2025-06-12 16:54:32 +0200

    refactor population 1) delete the InputPop 2) refactor naming and usage 3) add defense programming 4) change the logic of returns for the creation functions

--- include/toolbox/genetic/population.h
+++ include/toolbox/genetic/population.h
@@ -3,34 +3,76 @@
 
 #include <stddef.h>
 
-// strucute used to store all input values needed to create popualtion
-typedef struct InputPop {
-    int cols;  // number of columns  of population
-    int rows;  // number of rows of population
-    float **S; // matrix of max and mins of each collumns in populations
-}InputPop;
-
-// structure used to store population 
-typedef struct Pop {
-    float **pop; // population matrix 
-    float **S;   // matrix of max and min for each collumn 
+// structure used to store population
+typedef struct Population {
+    float **pop; // population matrix
+    float **S;   // matrix of max and min for each column
     int cols;    // number of columns  of population
     int rows;    // number of rows of population
-}Pop;
+}Population;
 
-// function to create population from InputPop structure 
-void createStructure(struct InputPop *input,  struct Pop *pop);
+/*
+ * This function is used to create population from InputPop structure
+ * Input:
+*     InputPop *inputPop - the inputPop struct pointer
+ *    float* max -.the pointer to float array of max matrix
+ *    float* min -.the pointer to float array of min matrix
+ *    int* size - the pointer to int array of population size matrix
+ * Output;
+ *    Pop* - pointer to the created population
+ */
+Population* createFilledPopulation(const float* max, const float* min, const int* size);
 
-// function to clear population
-void clearPopulation(struct Pop* population);
+/*
+ * This function is used to clear population
+ * Input:
+ *    Pop *population - the population struct pointer
+ * Output;
+ *    There is no output in the function, but the population is manipulated instead
+ */
+void clearPopulation(Population* population);
 
-// function to create InputPop for future pop
-void createInputPop(struct InputPop *inputPop, float* max, float* min, int* size);
+/*
+ * This function is used to copy rows from the source to pop between two indexes in the index array, not including the second one
+ * Input:
+ *    Pop *population - the population struct pointer
+ *    Pop *source - the population struct pointer which is used as a source of the rows
+ *    int *indexes - the pointer to int array of list of intended to be copied rows
+ * Output;
+ *    There is no output in the function, but the population is manipulated instead
+ */
+void copyPartOfPop(const Population *population, const Population *source, const int *indexes);
 
-// function to copy rows from the source to pop between two indexes in the indexes array not including second one
-void placePartOfPop(struct Pop *pop, struct Pop *source, int *indexes);
+/*
+ * This function is used to re-generate random population in the size matrix
+ * Input:
+ *    Pop *population - the population struct pointer
+ * Output;
+ *    There is no output in the function, but the population is manipulated instead
+ */
+void generateRandomPopulation(const Population *population);
 
-// function to re-generate random population in the size matrix
-void generateRandomPopulation(struct Pop *population);
+#define CLEAR_MATRIX_UNTIL(matrix, count_to_free) do { \
+    if (matrix != NULL) {                              \
+        for (int i = 0; i < (count_to_free); i++) {    \
+            if (matrix[i] != NULL) {                   \
+                free(matrix[i]);                       \
+                matrix[i] = NULL;                      \
+            }                                          \
+        }                                              \
+    }                                                  \
+} while (0)
+
+// macro to make redundant check of the inputPop or pop
+#define ASSERT_POPULATION(item) do {                                                            \
+    assert(item != NULL &&  "population(input) pointer should not be empty!");                     \
+    assert(item->S != NULL &&  "population(input) size matrix should not be empty!");              \
+                                                                                                \
+    assert(item->S[0] != NULL &&  "population(input) size max array should not be empty!");        \
+    assert(item->S[1] != NULL &&  "population(input) size min array matrix should not be empty!"); \
+                                                                                                \
+    assert(item->rows > 0 && "Population(input) rows must be positive");                        \
+    assert(item->cols > 0 && "Population(input) columns must be positive");                     \
+} while (0);
 
 #endif
--- src/toolbox/genetic/population.c
+++ src/toolbox/genetic/population.c
@@ -1,98 +1,116 @@
 #include "genetic/population.h"
-
 #include "general/general_math.h"
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <stddef.h>
 #include <string.h>
 
-static void clearInputPop(struct InputPop* input){
-  if(input){
-    free(input->S[0]);
-    free(input->S[1]);
-    free(input->S);
-    free(input);
+void clearPopulation(Population* population) {
+  printf("delete\n");
+  fflush(stdout);
+  if (population == NULL) { return; }
+  assert(population->pop && population->S);
+
+  // free the pop part
+  for (int i = 0; i < population->rows; i++) {
+    free(population->pop[i]);
   }
+  free(population->pop);
+
+  // free the S part
+  free(population->S[0]);
+  free(population->S[1]);
+  free(population->S);
+
+  // free population struct
+  free(population);
 }
 
+static Population* createPopulationFromData(const float* max, const float* min, const int* size){
+  assert(max  != NULL &&  "Max values array should not be empty!");
+  assert(min  != NULL &&  "Min values array should not be empty!");
+  assert(size != NULL &&  "Size values array should not be empty!");
 
-void createInputPop(struct InputPop *inputPop, float* max, float* min, int* size){
-  
-  inputPop->cols = size[1];
-  inputPop->rows = size[0];
+  assert(size[0] > 0 && "Population rows must be positive");
+  assert(size[1] > 0 && "Population columns must be positive");
 
-  inputPop->S    = (float**)malloc(2 * sizeof(float*));
-  inputPop->S[0] = (float* )malloc(inputPop->cols * sizeof(float));
-  inputPop->S[1] = (float* )malloc(inputPop->cols * sizeof(float));
+  Population *population = NULL;
+  population = malloc(sizeof(Population));
+  if (population == NULL){ perror("Failed to allocate Population struct"); exit(EXIT_FAILURE); }
 
-  memcpy(inputPop->S[0], max, inputPop->cols * sizeof(float));
-  memcpy(inputPop->S[1], min, inputPop->cols * sizeof(float));
-}
+  population->rows = size[0];
+  population->cols = size[1];
 
-static void alocatePopulation(struct InputPop* input, struct Pop* population){
-  population->cols = input->cols;
-  population->rows = input->rows;
+  population->S = NULL;
+  population->pop = NULL;
 
   population->pop = (float **)malloc(population->rows * sizeof(float *));
+  if (population->pop == NULL) { perror("Failed to allocate Population matrix"); free(population); exit(EXIT_FAILURE); }
+
   for (int i = 0; i < population->rows; i++) {
+    population->pop[i] = NULL;
     population->pop[i] = (float *)malloc(population->cols * sizeof(float));
+    if (population->pop[i] == NULL) { perror("Failed to allocate Population "
+                                             "matrix row"); CLEAR_MATRIX_UNTIL(population->pop, i); free(population->pop); free(population); exit(EXIT_FAILURE); }
   }
 
   population->S    = (float**)malloc(2 * sizeof(float*));
+  if (population->S == NULL) { perror("Failed to allocate Population "
+                                      "Size matrix"); CLEAR_MATRIX_UNTIL(population->pop, population->rows); free(population->pop); free(population); exit(EXIT_FAILURE);}
+
+  population->S[0] = NULL;
+  population->S[1] = NULL;
+
   population->S[0] = (float* )malloc(population->cols * sizeof(float));
+  if (population->S[0] == NULL) { perror("Failed to allocate Population Size min array");
+                                  free(population->S); CLEAR_MATRIX_UNTIL(population->pop, population->rows); free(population->pop); free(population); exit(EXIT_FAILURE);}
+
   population->S[1] = (float* )malloc(population->cols * sizeof(float));
+  if (population->S[1] == NULL) { perror("Failed to allocate Population Size max array");
+                                  free(population->S[0]); free(population->S); CLEAR_MATRIX_UNTIL(population->pop, population->rows); free(population->pop);
+                                  free(population); exit(EXIT_FAILURE);}
 
-  memcpy(population->S[0], input->S[0], population->cols * sizeof(float));
-  memcpy(population->S[1], input->S[1], population->cols * sizeof(float));
 
-  // clear the input pop
-  clearInputPop(input);
+  memcpy(population->S[0], max, population->cols * sizeof(float));
+  memcpy(population->S[1], min, population->cols * sizeof(float));
+
+  return population;
 }
 
-void clearPopulation(struct Pop* population) {
-  if (population) {
-    // free the pop part
-    for (int i = 0; i < population->rows; i++) {
-      free(population->pop[i]);
-    }
-    free(population->pop);
-
-    // free the S part
-    free(population->S[0]);
-    free(population->S[1]);
-    free(population->S);
-
-    // free population struct
-    free(population);
-  }
-}
-
-void createStructure(struct InputPop *input, struct Pop *population) {
-
-  alocatePopulation(input, population);
-
-  generateRandomPopulation(population);
-}
-
-void generateRandomPopulation(struct Pop *population){
-  float newValue;
+void generateRandomPopulation(const Population *population){
+  ASSERT_POPULATION(population);
+  assert(population->pop != NULL &&  "population pop matrix should not be empty!");
+  assert(population->S != NULL &&  "population S array should not be empty!"); // hack to prevent warning; This is done i macro
 
   for(int y = 0; y < population->rows; y++){
     for(int i=0; i < population->cols; i++ ){
-      
-      newValue = createRandomFloat(population->S[1][i], population->S[0][i]);
+
+      const float newValue = createRandomFloat(population->S[1][i], population->S[0][i]);
       population->pop[y][i] = newValue;
     }
   }
 }
 
-void placePartOfPop(struct Pop *pop, struct Pop *source, int *indexes){
+Population* createFilledPopulation(const float* max, const float* min, const int* size) {
+
+  Population *population = createPopulationFromData(max, min, size);
+  generateRandomPopulation(population);
+
+  return population;
+}
+
+void copyPartOfPop(const Population *population, const Population *source, const int *indexes){
+  ASSERT_POPULATION(population);
+  assert(population->pop != NULL &&  "population pop matrix should not be empty!");
+
+  ASSERT_POPULATION(source);
+  assert(source->pop != NULL &&  "source pop matrix should not be empty!");
+
   // the rows of the source are placed into pop between two indexes in the indexes, not including the second index
   int globalIndex = 0;
   for(int i=indexes[0]; i<indexes[1]; i++){
-    memcpy(pop->pop[i], source->pop[globalIndex], source->cols * sizeof(float));
+    memcpy(population->pop[i], source->pop[globalIndex], source->cols * sizeof(float));
     globalIndex++;
   }
 }
